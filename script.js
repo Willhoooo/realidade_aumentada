48270fd6082197d2ae169e758e34e315dfa54ce4
William Mattson Kheel Gomes Araújo <157336038+Willhoooo@users.noreply.github.com>
2024-12-17 11:16:20 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-  .then(stream => {
-    const video = document.querySelector('video');
-    video.srcObject = stream;
-  })
-  .catch(error => {
-    console.error('Erro ao acessar a câmera e o microfone:', error);
-  });
